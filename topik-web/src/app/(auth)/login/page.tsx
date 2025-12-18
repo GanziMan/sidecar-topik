b33f22d68dc1441e6a43b3c26b1a39e312174db4
bumsu_kim <bumsu_kim@daekyo.co.kr>
2025-12-18 11:48:09 +0900

    chore: add dynamic export to Home and LoginPage components for improved rendering behavior in TOPIK web application

--- topik-web/src/app/(auth)/login/page.tsx
+++ topik-web/src/app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 import { LoginForm } from "./_components/LoginForm";
 
+export const dynamic = "force-dynamic";
+
 export default function LoginPage() {
   return <LoginForm className="w-full max-w-md mx-auto flex flex-col justify-center h-screen" />;
 }
