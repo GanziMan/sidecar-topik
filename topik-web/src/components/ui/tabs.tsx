338057fee5c5388d26b07835ccd25dc2c4a38756
bumsu_kim <bumsu_kim@daekyo.co.kr>
2025-12-29 15:59:02 +0900

    refactor: update Tabs component styles for improved layout
    - Adjusted the class names in TabsList to include margin and border styles for better visual separation.
    - Modified TabsContent to reset the top margin, enhancing the overall layout consistency.

--- topik-web/src/components/ui/tabs.tsx
+++ topik-web/src/components/ui/tabs.tsx
@@ -13,7 +13,10 @@ const TabsList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
-    className={cn("inline-flex h-9 items-center justify-start p-1 text-muted-foreground w-full", className)}
+    className={cn(
+      "inline-flex h-9 items-center justify-start p-1 text-muted-foreground w-full mt-[60px] border-b border-b-gray-200 focus:ring-0",
+      className
+    )}
     {...props}
   />
 ));
@@ -41,7 +44,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-2 ring-offset-background  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      "mt-0 ring-offset-background  focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
       className
     )}
     {...props}
